2918f5a554748c991c847ebcb45bba99bb73cb0e
Rostyslav <rostislavkpi@gmail.com>
2026-07-27 01:35:51 +0300

    add swagger and finish setup

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { CoreModule } from './core/core.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(CoreModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Twitch clone app')
+    .setDescription('The Twitch clone API description')
+    .setVersion('1.0')
+    .addTag('twitch')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
