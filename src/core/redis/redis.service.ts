bba98022ddda89d1753a17c74cdaf598b363403a
Rostyslav <rostislavkpi@gmail.com>
2026-07-27 19:36:45 +0300

    add metadata to redis record

--- src/core/redis/redis.service.ts
+++ src/core/redis/redis.service.ts
@@ -1,10 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleDestroy } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { Redis } from 'ioredis';
+import { createClient, type RedisClientType } from 'redis';
 
 @Injectable()
-export class RedisService extends Redis {
+export class RedisService implements OnModuleDestroy {
+  public readonly client: RedisClientType<any, any, any, 2, any>;
+
   public constructor(private readonly configService: ConfigService) {
-    super(configService.getOrThrow<string>('REDIS_URI'));
+    this.client = createClient({
+      url: configService.getOrThrow<string>('REDIS_URI'),
+      RESP: 2,
+      socket: {
+        reconnectStrategy: (retries) => Math.min(retries * 50, 500),
+      },
+    });
+
+    void this.client.connect().catch((err) => {
+      console.error('Redis client connect error:', err);
+    });
+  }
+
+  public async onModuleDestroy() {
+    try {
+      await this.client.disconnect();
+    } catch {
+      // ignore shutdown errors
+    }
   }
 }
