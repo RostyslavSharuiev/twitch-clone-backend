f8d917903a938c109ae106dcb89051613fe52d0d
Rostyslav <rostislavkpi@gmail.com>
2026-08-03 21:17:00 +0300

    Add and implement follow res

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -13,6 +13,7 @@ import { VerificationModule } from '@/src/modules/auth/verification/verification
 import { CategoryModule } from '@/src/modules/category/category.module';
 import { ChatModule } from '@/src/modules/chat/chat.module';
 import { CronModule } from '@/src/modules/cron/cron.module';
+import { FollowModule } from '@/src/modules/follow/follow.module';
 import { LivekitModule } from '@/src/modules/libs/livekit/livekit.module';
 import { MailModule } from '@/src/modules/libs/mail/mail.module';
 import { StorageModule } from '@/src/modules/libs/storage/storage.module';
@@ -61,6 +62,7 @@ import { RedisModule } from './redis/redis.module';
     WebhookModule,
     CategoryModule,
     ChatModule,
+    FollowModule,
   ],
 })
 export class CoreModule {}
