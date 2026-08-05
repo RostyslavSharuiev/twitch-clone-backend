38136d4a9013a6022f7eea4ed6a99ea50cead319
Rostyslav <rostislavkpi@gmail.com>
2026-08-05 12:38:27 +0300

    Add plan logic

---
+++ src/modules/sponsorship/plan/models/plan.model.ts
@@ -0,0 +1,37 @@
+import { Field, ID, ObjectType } from '@nestjs/graphql';
+
+import type { SponsorshipPlan } from '@/src/generated/prisma/client';
+import { UserModel } from '@/src/modules/auth/account/models/user.model';
+
+@ObjectType()
+export class PlanModel implements SponsorshipPlan {
+  @Field(() => ID)
+  public id!: string;
+
+  @Field(() => Date)
+  public createdAt!: Date;
+
+  @Field(() => Date)
+  public updatedAt!: Date;
+
+  @Field(() => String)
+  public title!: string;
+
+  @Field(() => String, { nullable: true })
+  public description!: string | null;
+
+  @Field(() => Number)
+  public price!: number;
+
+  @Field(() => String)
+  public stripeProductId!: string;
+
+  @Field(() => String)
+  public stripePlanId!: string;
+
+  @Field(() => UserModel)
+  public channel!: UserModel;
+
+  @Field(() => String)
+  public channelId!: string;
+}
